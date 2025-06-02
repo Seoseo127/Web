2a1fd9b7f01a9f06872c642246d350092b352294
Seoseo127 <ch00127@naver.com>
2025-06-02 18:01:23 +0900

    Add files via upload

---
+++ 04_JDBC/WebContent/employee/editForm.jsp
@@ -0,0 +1,64 @@
+<%@page import="com.mystudy.dao.EmployeeDAO"%>
+<%@page import="com.mystudy.common.JdbcUtil"%>
+<%@page import="com.mystudy.vo.EmployeeVO"%>
+<%@page import="java.sql.*"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%--(실습) 전달받은 데이터를 이용해서 DB에서 데이터 조회하고 화면에 표시 --%>
+<%
+	//1. 파라미터 값 추출, 확인(sabun)
+	int sabun = Integer.parseInt(request.getParameter("sabun"));
+
+	//2. DB 데이터 조회 - sabun 과 일치하는 데이터 찾기(SELECT)
+	EmployeeDAO dao = new EmployeeDAO();
+	EmployeeVO vo = dao.selectById(sabun);
+	System.out.println("vo : " + vo);
+
+	//3. 데이터 화면 출력
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>수정화면</title>
+</head>
+<body>
+	<h1>수정화면[ editForm.jsp ]</h1>
+	<form action="update.jsp" method="get">
+		<table>
+			<tr>
+				<th>사번</th>
+				<td>
+					<input type="text" name="sabun" value="<%=vo.getSabun() %>" readonly>
+				</td>
+			</tr>
+			<tr>
+				<th>성명</th>
+				<td>
+					<input type="text" name="name" value="<%=vo.getName() %>">
+				</td>
+			</tr>
+			<tr>
+				<th>금액</th>
+				<td>
+					<input type="number" name="pay" value="<%=vo.getPay() %>">
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<input type="submit" value="수 정">
+					<input type="reset" value="초기화">
+				</td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
+
+
+
+
+
+
+
+
---
+++ 04_JDBC/src/com/mystudy/dao/EmployeeDAO.java
@@ -0,0 +1,108 @@
+package com.mystudy.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.mystudy.common.JdbcUtil;
+import com.mystudy.vo.EmployeeVO;
+
+public class EmployeeDAO {
+	private static final String SELECT_BY_ID = 
+			"SELECT SABUN, NAME, REGDATE, PAY " +
+			"FROM EMPLOYEE " +
+			"WHERE SABUN = ? ";
+
+	public List<EmployeeVO> select() {
+		return null;
+	}
+	
+	public EmployeeVO selectById(int sabun) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		EmployeeVO vo = null;
+		try {
+			conn = JdbcUtil.getConnection();
+			pstmt = conn.prepareStatement(SELECT_BY_ID);
+			pstmt.setInt(1, sabun);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				vo = new EmployeeVO(
+						rs.getInt("SABUN"),
+						rs.getString("NAME"),
+						rs.getDate("REGDATE"),
+						rs.getInt("PAY") );
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtil.close(conn, pstmt, rs);
+		}	
+		return vo;
+	}
+	
+	public int insert(EmployeeVO vo) {
+		return 0;
+	}
+	
+	public int update(EmployeeVO vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = JdbcUtil.getConnection();
+			
+			StringBuilder sql = new StringBuilder();
+			sql.append("UPDATE EMPLOYEE ");
+			sql.append("   SET NAME = ? "); // 1
+			sql.append("     , PAY = ? ");  // 2
+			sql.append("     , REGDATE = SYSDATE ");
+			sql.append("WHERE SABUN = ? "); // 3
+			pstmt = conn.prepareStatement(sql.toString());
+			
+			pstmt.setString(1, vo.getName());
+			pstmt.setInt(2, vo.getPay());
+			pstmt.setInt(3, vo.getSabun());
+			
+			return pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
+	public int delete(int sabun) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		int result = 0;
+		try {
+			conn = JdbcUtil.getConnection();
+			
+			String sql = "DELETE FROM EMPLOYEE WHERE SABUN = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, sabun);
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			result = -1;
+		}
+		
+		return result;
+	}
+	
+	public int delete(EmployeeVO vo) {
+		return 0;
+	}
+	
+}
+
+
+
